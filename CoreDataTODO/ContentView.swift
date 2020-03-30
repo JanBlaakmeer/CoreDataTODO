d49adb72d5bf94913593bcdaf89fa70eda73c775
Jan Blaakmeer <jan@blaakmeer.nl>
2020-03-30 16:17:23 +0200

    Initial Commit

---
+++ CoreDataTODO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataTODO
+//
+//  Created by Jan Blaakmeer on 30/03/2020.
+//  Copyright © 2020 Jan Blaakmeer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
